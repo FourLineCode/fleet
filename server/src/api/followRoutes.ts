65f3baadb189861c4633dec62507d42c599222a0
ItzAkmal <akmal3535.ah@gmail.com>
2021-01-08 05:59:22 +0600

    follow routes working i think

--- server/src/api/followRoutes.ts
+++ server/src/api/followRoutes.ts
@@ -1,31 +1,32 @@
 import { Router } from 'express'
 import { StatusCodes } from 'http-status-codes'
+import { getManager } from 'typeorm'
+import Follow from '../entity/Follow'
+import User from '../entity/User'
 import auth from '../middlewares/auth'
-import Follow from '../models/follow'
-import User, { UserType } from '../models/user'
 
 const router = Router()
 
 // Follow a user
 router.post('/:id', auth, async (req, res, next) => {
 	try {
-		const { id } = req.params
-		const followedUser = await User.findOne({ _id: id })
+		const followedUser = await User.findOne({ id: req.params.id })
 		if (!followedUser) {
 			res.status(StatusCodes.BAD_REQUEST)
 			throw new Error('Requested user doesnt exist')
 		}
 
-		const alreadyFollows = await Follow.findOne({ from: req.userId, to: id })
+		const alreadyFollows = await Follow.findOne({ where: { from: req.userId, to: req.params.id } })
 		if (alreadyFollows) {
 			res.status(StatusCodes.BAD_REQUEST)
 			throw new Error('You already follow this user')
 		}
 
 		const follow = await Follow.create({
-			from: req.userId,
-			to: id,
+			from: req.user,
+			to: followedUser,
 		})
+
 		await follow.save()
 
 		res.status(StatusCodes.OK).json({ success: true })
@@ -37,20 +38,25 @@ router.post('/:id', auth, async (req, res, next) => {
 // Unfollow a user
 router.post('/unfollow/:id', auth, async (req, res, next) => {
 	try {
-		const { id } = req.params
-		const followedUser = await User.findOne({ _id: id })
+		const followedUser = await User.findOne({ id: req.params.id })
 		if (!followedUser) {
 			res.status(StatusCodes.BAD_REQUEST)
 			throw new Error('Requested user doesnt exist')
 		}
 
-		const alreadyFollows = await Follow.findOne({ from: req.userId, to: id })
+		const alreadyFollows = await Follow.findOne({ where: { from: req.userId, to: req.params.id } })
 		if (!alreadyFollows) {
 			res.status(StatusCodes.BAD_REQUEST)
 			throw new Error('You do not follow this user')
 		}
 
-		await Follow.findOneAndDelete({ from: req.userId, to: id })
+		await getManager()
+			.getRepository(Follow)
+			.createQueryBuilder('follow')
+			.delete()
+			.where('from = :from', { from: req.userId })
+			.andWhere('to = :to', { to: req.params.id })
+			.execute()
 
 		res.status(StatusCodes.OK).json({ success: true })
 	} catch (error) {
@@ -61,14 +67,13 @@ router.post('/unfollow/:id', auth, async (req, res, next) => {
 // Check if you follow a user
 router.get('/check/:id', auth, async (req, res, next) => {
 	try {
-		const { id } = req.params
-		const followedUser = await User.findOne({ _id: id })
+		const followedUser = await User.findOne({ id: req.params.id })
 		if (!followedUser) {
 			res.status(StatusCodes.BAD_REQUEST)
 			throw new Error('Requested user doesnt exist')
 		}
 
-		const alreadyFollows = await Follow.findOne({ from: req.userId, to: id })
+		const alreadyFollows = await Follow.findOne({ where: { from: req.userId, to: req.params.id } })
 		if (!alreadyFollows) {
 			return res.status(StatusCodes.OK).json({ follows: false })
 		}
@@ -82,15 +87,14 @@ router.get('/check/:id', auth, async (req, res, next) => {
 // Get follow counts
 router.get('/count/:id', auth, async (req, res, next) => {
 	try {
-		const { id } = req.params
-		const user = await User.findOne({ _id: id })
+		const user = await User.findOne({ where: { id: req.params.id }, relations: ['followers', 'following'] })
 		if (!user) {
 			res.status(StatusCodes.BAD_REQUEST)
 			throw new Error('Requested user doesnt exist')
 		}
 
-		const followers = await Follow.find({ to: id }).select('from to')
-		const following = await Follow.find({ from: id }).select('from to')
+		const followers = user.followers
+		const following = user.following
 
 		res.status(StatusCodes.OK).json({ followers, following })
 	} catch (error) {
@@ -101,30 +105,28 @@ router.get('/count/:id', auth, async (req, res, next) => {
 // Get follow users
 router.get('/users/:id', auth, async (req, res, next) => {
 	try {
-		const { id } = req.params
-		const user = await User.findOne({ _id: id })
+		const user = await getManager()
+			.getRepository(User)
+			.createQueryBuilder('user')
+			.where('user.id = :id', { id: req.params.id })
+			.leftJoinAndSelect('user.followers', 'followers')
+			.leftJoinAndSelect('followers.from', 'frfrom')
+			.leftJoinAndSelect('followers.to', 'frto')
+			.leftJoinAndSelect('user.following', 'following')
+			.leftJoinAndSelect('following.from', 'fnfrom')
+			.leftJoinAndSelect('following.to', 'fnto')
+			.getOne()
+
 		if (!user) {
 			res.status(StatusCodes.BAD_REQUEST)
 			throw new Error('Requested user doesnt exist')
 		}
 
-		const followers = await Follow.find({ to: id }).select('from')
-		const followerUsers: (UserType | null)[] = []
+		const followers = user.followers.map((follow) => follow.from)
 
-		for (const follow of followers) {
-			const follower = await User.findOne({ _id: follow.from })
-			followerUsers.push(follower)
-		}
+		const following = user.following.map((follow) => follow.to)
 
-		const following = await Follow.find({ from: id }).select('to')
-		const followingUsers: (UserType | null)[] = []
-
-		for (const follow of following) {
-			const followed = await User.findOne({ _id: follow.to })
-			followingUsers.push(followed)
-		}
-
-		res.status(StatusCodes.OK).json({ followers: followerUsers, following: followingUsers })
+		res.status(StatusCodes.OK).json({ followers, following })
 	} catch (error) {
 		next(error)
 	}
