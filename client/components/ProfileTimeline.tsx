8f71f5584ca73baf6f472f2133036459bb04a0a2
ItzAkmal <akmal3535.ah@gmail.com>
2021-01-19 11:30:29 +0600

    profile timeline progress bug fixed

--- client/components/ProfileTimeline.tsx
+++ client/components/ProfileTimeline.tsx
@@ -45,17 +45,20 @@ const ProfileTimeline = () => {
 					<div className='text-xl font-bold text-center text-white'>Posts</div>
 				</div>
 			</div>
-			<div className={clsx(isLoading && 'justify-center', 'h-full my-4 flex-col space-y-4')}>
-				{!isLoading && data && data.length > 0 ? (
-					data && data.map((fleet: FleetType) => <Fleet fleet={fleet} key={fleet.id} />)
-				) : (
-					<div className='flex items-center justify-center w-full h-full'>
-						<div className='flex-col'>
-							<ErrorIcon className='w-20 h-20 mx-auto text-gray-500' />
-							<div className='text-2xl font-semibold text-gray-500'>No Fleets found</div>
+			<div
+				className={clsx(isLoading && 'justify-center items-center flex', 'flex h-full my-4 flex-col space-y-4')}
+			>
+				{!isLoading &&
+					(data && data.length > 0 ? (
+						data && data.map((fleet: FleetType) => <Fleet fleet={fleet} key={fleet.id} />)
+					) : (
+						<div className='flex items-center justify-center w-full h-full'>
+							<div className='flex-col'>
+								<ErrorIcon className='w-20 h-20 mx-auto text-gray-500' />
+								<div className='text-2xl font-semibold text-gray-500'>No Fleets found</div>
+							</div>
 						</div>
-					</div>
-				)}
+					))}
 				{isLoading && (
 					<CircularProgress color='primary' variant='indeterminate' disableShrink size={30} thickness={4} />
 				)}
