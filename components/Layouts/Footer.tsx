eb2dfd940c913caf95530d46368b57a1dc3a7a9d
ItzAkmal <akmal3535.ah@gmail.com>
2021-02-14 21:49:45 +0600

    footer added

---
+++ components/Layouts/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<footer className='z-40 flex flex-col items-center justify-center w-full h-20 mb-16 text-gray-600 border-t md:mb-0 dark:text-gray-400 border-dark-500 bg-light dark:bg-dark-800'>
+			<div>Copyright © 2021 FourLineCode Inc.</div>
+			<div>All Rights Reserved</div>
+		</footer>
+	)
+}
+
+export default Footer
