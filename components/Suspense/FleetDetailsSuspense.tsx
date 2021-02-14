a94ea6ee2a1bfcf249377c38351dec58315fd764
ItzAkmal <akmal3535.ah@gmail.com>
2021-02-14 21:58:43 +0600

    suspense bg lighter

--- components/Suspense/FleetDetailsSuspense.tsx
+++ components/Suspense/FleetDetailsSuspense.tsx
@@ -7,30 +7,30 @@ const FleetDetailsSuspense = () => {
 				<div className='w-full'>
 					<div className='flex flex-col pb-2 space-x-1 space-y-2 border-b border-dark-700'>
 						<div className='flex mt-1'>
-							<div className='mr-2 bg-gray-600 rounded-lg w-14 h-14' />
+							<div className='mr-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-14 h-14' />
 							<div className='space-y-1'>
-								<div className='w-32 h-5 bg-gray-600 rounded' />
-								<div className='w-20 h-4 bg-gray-600 rounded' />
+								<div className='w-32 h-5 bg-gray-300 rounded dark:bg-gray-600' />
+								<div className='w-20 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 						<div className='space-y-2'>
-							<div className='w-full h-5 bg-gray-600 rounded' />
-							<div className='w-full h-5 bg-gray-600 rounded' />
-							<div className='w-2/3 h-5 bg-gray-600 rounded' />
+							<div className='w-full h-5 bg-gray-300 rounded dark:bg-gray-600' />
+							<div className='w-full h-5 bg-gray-300 rounded dark:bg-gray-600' />
+							<div className='w-2/3 h-5 bg-gray-300 rounded dark:bg-gray-600' />
 						</div>
 						<div className='flex space-x-2'>
-							<div className='w-20 h-3 bg-gray-600 rounded' />
-							<div className='h-3 bg-gray-600 rounded w-28' />
+							<div className='w-20 h-3 bg-gray-300 rounded dark:bg-gray-600' />
+							<div className='h-3 bg-gray-300 rounded dark:bg-gray-600 w-28' />
 						</div>
 					</div>
 					<div className='w-full h-8 px-2 mt-1'>
 						<div className='flex items-center w-full h-full'>
 							<div className='flex items-center flex-1 justify-evenly'>
 								<div className='flex items-center justify-evenly'>
-									<div className='w-10 h-5 bg-gray-600 rounded' />
+									<div className='w-10 h-5 bg-gray-300 rounded dark:bg-gray-600' />
 								</div>
 								<div className='flex items-center'>
-									<div className='w-10 h-5 bg-gray-600 rounded' />
+									<div className='w-10 h-5 bg-gray-300 rounded dark:bg-gray-600' />
 								</div>
 							</div>
 						</div>
@@ -41,49 +41,49 @@ const FleetDetailsSuspense = () => {
 				{/* reply */}
 				<div className='w-full px-2 py-2 mb-3 border rounded-lg shadow-xl border-dark-700'>
 					<div className='flex space-x-1'>
-						<div className='w-12 h-12 mt-1 mr-2 bg-gray-600 rounded-lg' />
+						<div className='w-12 h-12 mt-1 mr-2 bg-gray-300 rounded-lg dark:bg-gray-600' />
 						<div className='w-full'>
 							<div className='space-y-1'>
 								<div className='flex items-center justify-between w-full'>
 									<div className='flex items-center space-x-1'>
 										<div className='flex items-center space-x-2'>
-											<span className='w-24 h-4 bg-gray-600 rounded' />
-											<span className='w-16 h-4 bg-gray-600 rounded' />
+											<span className='w-24 h-4 bg-gray-300 rounded dark:bg-gray-600' />
+											<span className='w-16 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 										</div>
 										<span className='text-white'>{' • '}</span>
-										<span className='w-20 h-4 bg-gray-600 rounded' />
+										<span className='w-20 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 									</div>
 								</div>
-								<div className='w-24 h-3 bg-gray-600 rounded' />
+								<div className='w-24 h-3 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 							<div className='py-2 space-y-1'>
-								<div className='w-full h-4 bg-gray-600 rounded' />
-								<div className='w-full h-4 bg-gray-600 rounded' />
-								<div className='w-1/3 h-4 bg-gray-600 rounded' />
+								<div className='w-full h-4 bg-gray-300 rounded dark:bg-gray-600' />
+								<div className='w-full h-4 bg-gray-300 rounded dark:bg-gray-600' />
+								<div className='w-1/3 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 					</div>
 				</div>
 				<div className='w-full px-2 py-2 mb-3 border rounded-lg shadow-xl border-dark-700'>
 					<div className='flex space-x-1'>
-						<div className='w-12 h-12 mt-1 mr-2 bg-gray-600 rounded-lg' />
+						<div className='w-12 h-12 mt-1 mr-2 bg-gray-300 rounded-lg dark:bg-gray-600' />
 						<div className='w-full'>
 							<div className='space-y-1'>
 								<div className='flex items-center justify-between w-full'>
 									<div className='flex items-center space-x-1'>
 										<div className='flex items-center space-x-2'>
-											<span className='w-20 h-4 bg-gray-600 rounded' />
-											<span className='h-4 bg-gray-600 rounded w-14' />
+											<span className='w-20 h-4 bg-gray-300 rounded dark:bg-gray-600' />
+											<span className='h-4 bg-gray-300 rounded dark:bg-gray-600 w-14' />
 										</div>
 										<span className='text-white'>{' • '}</span>
-										<span className='w-16 h-4 bg-gray-600 rounded' />
+										<span className='w-16 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 									</div>
 								</div>
-								<div className='w-24 h-3 bg-gray-600 rounded' />
+								<div className='w-24 h-3 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 							<div className='py-2 space-y-1'>
-								<div className='w-full h-4 bg-gray-600 rounded' />
-								<div className='w-2/3 h-4 bg-gray-600 rounded' />
+								<div className='w-full h-4 bg-gray-300 rounded dark:bg-gray-600' />
+								<div className='w-2/3 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 					</div>
--- components/Suspense/TimelineSuspense.tsx
+++ components/Suspense/TimelineSuspense.tsx
@@ -5,17 +5,17 @@ const TimelineSuspense = () => {
 		<>
 			<div className='w-full px-2 pt-2 border rounded-lg shadow-xl border-dark-700 animate-pulse lg:mx-auto lg:w-3/4'>
 				<div className='flex space-x-1'>
-					<div className='flex-shrink-0 w-12 h-12 mr-2 bg-gray-600 rounded-lg' />
+					<div className='flex-shrink-0 w-12 h-12 mr-2 bg-gray-300 rounded-lg dark:bg-gray-600' />
 					<div className='w-full space-y-2'>
 						<div className='flex items-center space-x-1'>
 							<div className='flex items-center space-x-1'>
-								<div className='h-4 bg-gray-600 rounded w-28' />
-								<div className='w-20 h-4 bg-gray-600 rounded' />
+								<div className='h-4 bg-gray-300 rounded dark:bg-gray-600 w-28' />
+								<div className='w-20 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 						<div className='space-y-1'>
-							<div className='w-full h-4 bg-gray-600 rounded' />
-							<div className='w-2/3 h-4 bg-gray-600 rounded' />
+							<div className='w-full h-4 bg-gray-300 rounded dark:bg-gray-600' />
+							<div className='w-2/3 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 						</div>
 					</div>
 				</div>
@@ -23,10 +23,10 @@ const TimelineSuspense = () => {
 					<div className='flex items-center w-full h-full'>
 						<div className='flex items-center flex-1 justify-evenly'>
 							<div className='flex items-center justify-evenly'>
-								<div className='w-8 h-4 bg-gray-600 rounded' />
+								<div className='w-8 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 							<div className='flex items-center'>
-								<div className='w-8 h-4 bg-gray-600 rounded' />
+								<div className='w-8 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 					</div>
@@ -34,18 +34,18 @@ const TimelineSuspense = () => {
 			</div>
 			<div className='w-full px-2 pt-2 border rounded-lg shadow-xl border-dark-700 animate-pulse lg:mx-auto lg:w-3/4'>
 				<div className='flex space-x-1'>
-					<div className='flex-shrink-0 w-12 h-12 mr-2 bg-gray-600 rounded-lg' />
+					<div className='flex-shrink-0 w-12 h-12 mr-2 bg-gray-300 rounded-lg dark:bg-gray-600' />
 					<div className='w-full space-y-2'>
 						<div className='flex items-center space-x-1'>
 							<div className='flex items-center space-x-1'>
-								<div className='h-4 bg-gray-600 rounded w-28' />
-								<div className='w-20 h-4 bg-gray-600 rounded' />
+								<div className='h-4 bg-gray-300 rounded dark:bg-gray-600 w-28' />
+								<div className='w-20 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 						<div className='space-y-1'>
-							<div className='w-full h-4 bg-gray-600 rounded' />
-							<div className='w-1/3 h-4 bg-gray-600 rounded' />
-							<div className='w-full h-64 bg-gray-600 rounded' />
+							<div className='w-full h-4 bg-gray-300 rounded dark:bg-gray-600' />
+							<div className='w-1/3 h-4 bg-gray-300 rounded dark:bg-gray-600' />
+							<div className='w-full h-64 bg-gray-300 rounded dark:bg-gray-600' />
 						</div>
 					</div>
 				</div>
@@ -53,10 +53,10 @@ const TimelineSuspense = () => {
 					<div className='flex items-center w-full h-full'>
 						<div className='flex items-center flex-1 justify-evenly'>
 							<div className='flex items-center justify-evenly'>
-								<div className='w-8 h-4 bg-gray-600 rounded' />
+								<div className='w-8 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 							<div className='flex items-center'>
-								<div className='w-8 h-4 bg-gray-600 rounded' />
+								<div className='w-8 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 					</div>
@@ -64,22 +64,22 @@ const TimelineSuspense = () => {
 			</div>
 			<div className='w-full px-2 pt-2 border rounded-lg shadow-xl border-dark-700 animate-pulse lg:mx-auto lg:w-3/4'>
 				<div className='flex space-x-1'>
-					<div className='flex-shrink-0 w-12 h-12 mr-2 bg-gray-600 rounded-lg' />
+					<div className='flex-shrink-0 w-12 h-12 mr-2 bg-gray-300 rounded-lg dark:bg-gray-600' />
 					<div className='w-full space-y-2'>
 						<div className='flex items-center space-x-1'>
 							<div className='flex items-center space-x-1'>
-								<div className='h-4 bg-gray-600 rounded w-28' />
-								<div className='w-20 h-4 bg-gray-600 rounded' />
+								<div className='h-4 bg-gray-300 rounded dark:bg-gray-600 w-28' />
+								<div className='w-20 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 						<div className='space-y-1'>
-							<div className='w-full h-4 bg-gray-600 rounded' />
-							<div className='w-1/3 h-4 bg-gray-600 rounded' />
+							<div className='w-full h-4 bg-gray-300 rounded dark:bg-gray-600' />
+							<div className='w-1/3 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							<div className='flex w-full h-64 space-x-1'>
-								<div className='w-2/3 h-full bg-gray-600 rounded' />
+								<div className='w-2/3 h-full bg-gray-300 rounded dark:bg-gray-600' />
 								<div className='flex flex-col flex-1 h-full space-y-1'>
-									<div className='flex-1 w-full bg-gray-600 rounded' />
-									<div className='flex-1 w-full bg-gray-600 rounded' />
+									<div className='flex-1 w-full bg-gray-300 rounded dark:bg-gray-600' />
+									<div className='flex-1 w-full bg-gray-300 rounded dark:bg-gray-600' />
 								</div>
 							</div>
 						</div>
@@ -89,10 +89,10 @@ const TimelineSuspense = () => {
 					<div className='flex items-center w-full h-full'>
 						<div className='flex items-center flex-1 justify-evenly'>
 							<div className='flex items-center justify-evenly'>
-								<div className='w-8 h-4 bg-gray-600 rounded' />
+								<div className='w-8 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 							<div className='flex items-center'>
-								<div className='w-8 h-4 bg-gray-600 rounded' />
+								<div className='w-8 h-4 bg-gray-300 rounded dark:bg-gray-600' />
 							</div>
 						</div>
 					</div>
