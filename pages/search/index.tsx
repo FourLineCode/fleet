c686b0b2bd13462d44053ed745b3c7170e564a19
ItzAkmal <akmal3535.ah@gmail.com>
2021-03-09 20:07:44 +0600

    idk man

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ProtectedLayout from '../../components/Layouts/ProtectedLayout'
+import ComingSoon from '../../components/Navigation/ComingSoon'
 import Menu from '../../components/Navigation/Menu'
-import Recommend from '../../components/Recommend/Recommend'
-import SearchResult from '../../components/Search/SearchResult'
 
 const Search = () => {
 	return (
 		<ProtectedLayout title='Search'>
 			<div className='grid min-h-screen grid-cols-4 bg-light dark:bg-dark-800 gap-x-1 xl:gap-x-4'>
 				<Menu />
-				<SearchResult />
-				<Recommend />
+				{/* <SearchResult />
+				<Recommend /> */}
+				<ComingSoon feature='Search' />
 			</div>
 		</ProtectedLayout>
 	)
