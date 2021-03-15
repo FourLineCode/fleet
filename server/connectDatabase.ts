86af5f4396387a52516a87a15152a18a4beeec36
ItzAkmal <akmal3535.ah@gmail.com>
2021-03-16 01:20:24 +0600

    i tried

--- server/connectDatabase.ts
+++ server/connectDatabase.ts
@@ -1,4 +1,3 @@
-import path from 'path'
 import { createConnection, getConnectionManager } from 'typeorm'
 
 export const connectDatabase = async () => {
@@ -6,15 +5,8 @@ export const connectDatabase = async () => {
 	const isAvailable = connectionManager.has('default')
 
 	if (!isAvailable) {
-		return await createConnection({
-			type: 'postgres',
-			url: process.env.DATABASE_URL,
-			logging: false,
-			synchronize: true,
-			entities: [path.resolve('/entity/**/*.ts')],
-		})
+		return await createConnection()
 	}
 
-	console.log(connectionManager.get('default').options)
 	return connectionManager.get('default')
 }
