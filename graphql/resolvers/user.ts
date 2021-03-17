c563d8acea6b11baaba95bb476a38805ca005840
ItzAkmal <akmal3535.ah@gmail.com>
2021-03-18 01:44:59 +0600

    better folder structure

--- graphql/resolvers/user.ts
+++ graphql/resolvers/user.ts
@@ -1,20 +1,5 @@
-import { extendType, objectType } from 'nexus'
+import { extendType } from 'nexus'
 import { Context } from '../context'
-
-export const User = objectType({
-	name: 'User',
-	definition: (t) => {
-		t.int('id')
-		t.string('email')
-		t.string('password')
-		t.string('username')
-		t.string('displayName')
-		t.string('bio')
-		t.boolean('isAdmin')
-		t.dateTime('createdAt')
-		t.dateTime('updatedAt')
-	},
-})
 
 export const UserQuery = extendType({
 	type: 'Query',
