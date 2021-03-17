c75a7fd5d362f894e31204d137233c7fe06fe20e
ItzAkmal <akmal3535.ah@gmail.com>
2021-03-17 21:38:03 +0600

    all user queries, custom auth checker added

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,16 +2,24 @@ import { ApolloServer } from 'apollo-server-micro'
 import { NextApiRequest, NextApiResponse } from 'next'
 import 'reflect-metadata'
 import { buildSchema } from 'type-graphql'
+import { authChecker } from '../../server/middlewares/authChecker'
 import { UserResolver } from '../../server/resolvers/userResolver'
 
 let apolloServerHandler: (req: any, res: any) => Promise<void>
 
-const getApolloServerHandler = async () => {
+const getApolloServerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 	if (!apolloServerHandler) {
 		const schema = await buildSchema({
 			resolvers: [UserResolver],
+			authChecker,
 		})
-		apolloServerHandler = new ApolloServer({ schema }).createHandler({
+		apolloServerHandler = new ApolloServer({
+			schema,
+			context: {
+				req,
+				res,
+			},
+		}).createHandler({
 			path: '/api/graphql',
 		})
 	}
@@ -19,7 +27,7 @@ const getApolloServerHandler = async () => {
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const apolloServerHandler = await getApolloServerHandler()
+	const apolloServerHandler = await getApolloServerHandler(req, res)
 
 	return apolloServerHandler(req, res)
 }
