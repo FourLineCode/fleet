354609bd597190b77f363a0a7dcc492a42899d6b
ItzAkmal <akmal3535.ah@gmail.com>
2021-03-18 01:36:34 +0600

    codegen files removed

--- lib/server/generated/resolvers/relations/User/args/index.ts
+++
@@ -1,5 +0,0 @@
-export { UserFleetArgs } from "./UserFleetArgs";
-export { UserFollowersArgs } from "./UserFollowersArgs";
-export { UserFollowingArgs } from "./UserFollowingArgs";
-export { UserLikeArgs } from "./UserLikeArgs";
-export { UserReplyArgs } from "./UserReplyArgs";
--- lib/server/generated/resolvers/relations/resolvers.index.ts
+++
@@ -1,5 +0,0 @@
-export { FleetRelationsResolver } from "./Fleet/FleetRelationsResolver";
-export { FollowRelationsResolver } from "./Follow/FollowRelationsResolver";
-export { LikeRelationsResolver } from "./Like/LikeRelationsResolver";
-export { ReplyRelationsResolver } from "./Reply/ReplyRelationsResolver";
-export { UserRelationsResolver } from "./User/UserRelationsResolver";
