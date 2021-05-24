b0f8d937ef625a206e5f719d23c27af2581ecf00
FourLineCode <akmal3535.ah@gmail.com>
2021-05-24 13:45:21 +0600

    coming soon component

--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,15 +1,11 @@
-import { Heading, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { ComingSooon } from '~components/layouts/ComingSoon';
 import { Layout } from '~components/layouts/Layout';
 
 const Messages = () => {
 	return (
 		<Layout authorized title='Messages' desc='Messages Page'>
-			<VStack>
-				<Heading bgGradient='linear(to-r, green.500, red.500)' bgClip='text'>
-					Messages Page
-				</Heading>
-			</VStack>
+			<ComingSooon />
 		</Layout>
 	);
 };
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,15 +1,11 @@
-import { Heading, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { ComingSooon } from '~components/layouts/ComingSoon';
 import { Layout } from '~components/layouts/Layout';
 
 const Settings = () => {
 	return (
 		<Layout authorized title='Settings' desc='Settings Page'>
-			<VStack>
-				<Heading bgGradient='linear(to-r, green.500, red.500)' bgClip='text'>
-					Settings Page
-				</Heading>
-			</VStack>
+			<ComingSooon />
 		</Layout>
 	);
 };
